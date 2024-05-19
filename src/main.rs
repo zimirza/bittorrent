e3ccaf8fb5f1b0fb2c7ac5325089b7dc5bcc47e5
Zishan Mirza <zishan.mirza@h-d-gmbh.de>
2024-05-19 09:46:35 +0200

    added enum and implementation for encoding and decoding

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::{collections::HashMap, sync::Arc};
+
+#[derive(Debug)]
+enum Bencode {
+    Integer(u64),
+    String(Arc<str>),
+    List(&'static [Bencode]),
+    Dictionary(HashMap<Arc<str>, Bencode>),
+}
+
+impl Bencode {
+    fn encode(&self) -> &[u8] {
+        match self {
+            Bencode::Integer(i) => {
+                todo!()
+            }
+            Bencode::String(s) => {
+                todo!()
+            }
+            Bencode::List(l) => {
+                todo!()
+            }
+            Bencode::Dictionary(e) => {
+                todo!()
+            }
+        }
+    }
+
+    fn decode(b: &[u8]) -> Result<(), Arc<str>> {
+        todo!()
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     Ok(())
